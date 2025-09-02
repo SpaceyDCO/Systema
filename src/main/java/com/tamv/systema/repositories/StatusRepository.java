f30b7e0fe6d7961a019cfab1f753fc1a8501b279
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-09-02 17:00:16 -0400

    - Create all Repositories for each entity along with a few utility methods that will be useful later on

---
+++ src/main/java/com/tamv/systema/repositories/StatusRepository.java
@@ -0,0 +1,12 @@
+package com.tamv.systema.repositories;
+
+import com.tamv.systema.entities.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status, Long> {
+    Optional<Status> findByName(String name);
+}
---
+++ src/main/java/com/tamv/systema/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tamv.systema.repositories;
+
+import com.tamv.systema.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
