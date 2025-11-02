c10e87a5b9e866dfd0e73d93d532b10cb48f5ec9
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-11-02 18:43:04 -0400

    Added Invoice controller and service

--- src/main/java/com/tamv/systema/repositories/StatusRepository.java
+++ src/main/java/com/tamv/systema/repositories/StatusRepository.java
@@ -1,6 +1,7 @@
 package com.tamv.systema.repositories;
 
 import com.tamv.systema.entities.Status;
+import com.tamv.systema.entities.StatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface StatusRepository extends JpaRepository<Status, Long> {
     Optional<Status> findByName(String name);
+    Optional<Status> findByNameAndType(String name, StatusType type);
 }
